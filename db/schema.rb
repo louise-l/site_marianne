fa421ecfeb2a2a20e3b0d874b94021d01abd4ca3
clement saad <saadclement@gmail.com>
2021-03-10 15:16:54 +0100

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_153651) do
+ActiveRecord::Schema.define(version: 2021_03_10_141459) do
 
   create_table "images", force: :cascade do |t|
     t.string "title"
