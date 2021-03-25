bb4fd95b10e517095e86ece7ec9e15137b805f41
louise longle <louise.longle@gmail.com>
2021-03-25 09:11:00 +0100

    begining to update the collage js

--- app/javascript/packs/components/carouselCollages.js
+++ app/javascript/packs/components/carouselCollages.js
@@ -1,31 +1,51 @@
 const carouselCollages = (carouselImgs) => {
   const slides = document.getElementsByClassName(carouselImgs)
+
+  // JE VERIFIE LA PRESENCE DE LA CLASSE DANS LA PAGE AVANT DE LANCER LA FONCTION 
   if (slides.length !== 0){
     let currentSlide = 1;
+    let currentZIndex = 1;
 
-  function showSlide(slideIndex) {
-    // CHANGE SLIDE
-    if (slideIndex > slides.length) { currentSlide = 1 }
-    if (slideIndex < 1) { currentSlide = slides.length }
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none'
+    const setPage = () => {
+      for (var i = 0; i < slides.length; i++) {
+        // on met toutes les slides en display none
+        slides[i].style.display = 'none'
+      }
+      // on display la slide en cours (-1 car on est avec des index)
+      slides[currentSlide - 1].style.display = 'flex'
     }
-    slides[currentSlide - 1].style.display = 'flex'
-  }
-  
-  
-  function nextSlide() {
-    showSlide(currentSlide += 1);
-  }
-  
-  function previousSlide() {
-    showSlide(currentSlide -= 1);
-  }
+    
 
-  showSlide(currentSlide);
+    function showSlide(slideIndex) {
+      console.log(slides[slides.length-1])
+
+      // GO TO THE BEGINING OF THE SLIDE IF NEEDED
+      if (slideIndex > slides.length) { currentSlide = 1 }
+      if (slideIndex < 1) { currentSlide = slides.length }
+      for (var i = 0; i < slides.length; i++) {
+        // on met toutes les slides en display none
+        //slides[i].style.display = 'none'
+      }
+      // on display la slide en cours (-1 car on est avec des index)
+      slides[currentSlide - 1].style.display = 'flex'
+    }
+    
+    
+    function nextSlide() {
+      showSlide(currentSlide += 1);
+    }
+    
+    function previousSlide() {
+      showSlide(currentSlide -= 1);
+    }
+
+    setPage(currentSlide);
+    // showSlide(currentSlide);
+
     document.getElementById('prev'+carouselImgs).addEventListener('click', () => {
       previousSlide();
     })
+
     document.getElementById('next'+carouselImgs).addEventListener('click',  () => {
       nextSlide();
     })
